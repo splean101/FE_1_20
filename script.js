7dc9a05b82c88b80a639b50f33c590d0322917be
splean101 <101splean@gmail.com>
2023-01-05 07:41:00 +0200

    done

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 //1.
 
-// let a = Number(prompt('number a', 'a'));
-// let b = Number(prompt('number b', 'b'));
+let a = Number(prompt('number a', 'a'));
+let b = Number(prompt('number b', 'b'));
 
-// function maxNumber(a, b) {
-//   if (a > b) {
-//     return a;
-//   } else if (b > a) {
-//     return b;
-//   } else {
-//     return 'a = b';
-//   }
-// }
+function maxNumber(a, b) {
+  if (a > b) {
+    return a;
+  } else if (b > a) {
+    return b;
+  } else {
+    return 'a = b';
+  }
+}
 
-// alert(maxNumber(a, b));
+alert(maxNumber(a, b));
 
 //2.
-// let num = Number(prompt('Enter the number'))
 
-// function reverse(number){
-//   return -number;
-// }
+let num = Number(prompt('Enter the number'));
 
-// alert(reverse(num))
+function reverse(number) {
+  return -number;
+}
+
+alert(reverse(num));
 
 //3.
+
 let number = prompt('Enter the number');
 let count = prompt('Enter the count');
 
@@ -38,3 +40,25 @@ function three(number, count) {
 alert(three(+number, +count));
 
 //4.
+
+let metric = prompt('enter the metric unit');
+let km = Number(prompt('enter kilometers'));
+
+function KmToM(kilometers) {
+  return kilometers * 1000;
+}
+function KmToCM(kilometers) {
+  return kilometers * 100000;
+}
+
+function getMetric(metric, km, m, cm) {
+  switch (metric) {
+    case 'm':
+      return m(km);
+      break;
+    case 'cm':
+      return cm(km);
+  }
+}
+
+alert(getMetric(metric, km, KmToM, KmToCM));
